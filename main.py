f1afc8369843e80ec0ecb609e055dd7f75c6124c
benjaminwyss <wyssbenjamin17@gmail.com>
2021-04-23 13:15:12 -0500

    create file skeletons for main and neuralNetworkRegressor

---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+import numpy as np
+import neuralNetworkRegressor as nnr
+
+print('test file works!')
---
+++ neuralNetworkRegressor.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class nnr:
+
+  def __init__self():
+    pass
+
+  def fit(self, X, Y):
+    self.X = X
+    self.Y = Y
+
+  def predict(self, X):
+    pass
+
+  def kFoldCrossValidation(self, X, Y, k=10):
+    pass
